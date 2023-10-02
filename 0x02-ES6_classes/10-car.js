097ddfb858b61f70bc002c87b97b2648279a187f
Patrick-052 <muiruripatrick052@gmail.com>
2023-10-02 22:57:59 +0300

    instance constructor

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+/* Implenting a class that implements a function that:
+* returns an object of the class
+*/
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
