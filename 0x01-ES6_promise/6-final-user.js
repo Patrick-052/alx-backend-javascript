c9829f4369f47dc7fddf5bfe72eb69af3c011bd2
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-06 01:35:07 +0300

    Update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([signUpUser, uploadPhoto]) => {
-      const reslist = [];
-      const obj1 = {
-        status: signUpUser.status,
-        value: signUpUser.value,
-      };
-      reslist.push(obj1);
-      const obj2 = {
-        status: uploadPhoto.status,
-        value: uploadPhoto.value,
-      };
-      reslist.push(obj2);
-      return reslist;
-    });
+  const user = await signUpUser(firstName, lastName).then((value) => ({
+    status: 'fulfilled',
+    value,
+  }));
+  const photo = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: `${error.name}: ${error.message}`,
+  }));
+  return [user, photo];
 }
