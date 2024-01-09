9be491a4b8ea3df25c6c92283538dc306622c2a6
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-10 00:18:56 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,13 @@
 the set values separated by a hyphen (-) */
 
 export default function cleanSet(set, startString) {
-
+  if (typeof startString !== 'string' || startString === '') return '';
+  const array = [];
+  set.forEach((item) => {
+    if (item.startsWith(startString)) {
+      const slicedString = startString === '' ? item : item.slice(startString.length);
+      array.push(slicedString);
+    }
+  });
+  return array.join('-');
 }
