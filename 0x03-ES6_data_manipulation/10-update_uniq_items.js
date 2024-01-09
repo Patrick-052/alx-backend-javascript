c6eb3a0c8146c055bf303342041e508f02a8383a
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-10 00:54:20 +0300

    Map operations update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
 /* Map operations */
 
 export default function updateUniqueItems(groceries) {
-	if (!(groceries instanceof Map)) throw TypeError('Cannot process');
-	groceries.forEach((value, key) => {
-		if (value === 1) groceries.set(key, 100);
-	});
-	return groceries;
+  if (!(groceries instanceof Map)) throw TypeError('Cannot process');
+  groceries.forEach((value, key) => {
+    if (value === 1) groceries.set(key, 100);
+  });
+  return groceries;
 }
