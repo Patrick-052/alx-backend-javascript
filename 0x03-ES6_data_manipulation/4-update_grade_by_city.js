9670de51acd3228eb7872f8ac1f8c6adb25ed09d
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-09 22:28:29 +0300

    code factorizing

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -20,25 +20,25 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 }
 
 
-/* Alternative solution: */
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  const SBC = students.filter((student) => student.location === city);
-  const SWG = SBC.map((student) => {
-    const grade = newGrades.filter((grade) => grade.studentId === student.id);
-    if (grade[0]) student.grade = grade[0].grade;
-    else student.grade = 'N/A';
-    return student;
-  });
-  return SWG;
-}
+// /* Alternative solution: */
+// export default function updateStudentGradeByCity(students, city, newGrades) {
+//   const SBC = students.filter((student) => student.location === city);
+//   const SWG = SBC.map((student) => {
+//     const grade = newGrades.filter((grade) => grade.studentId === student.id);
+//     if (grade[0]) student.grade = grade[0].grade;
+//     else student.grade = 'N/A';
+//     return student;
+//   });
+//   return SWG;
+// }
 
-/* Alternative solution: */
-export default function updateStudentGradeByCity1(students, city, newGrades) {
-  const studentsByCity = students.filter((student) => student.location === city);
-  const studentsWithGrades = studentsByCity.map((student) => {
-    const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
-    const grade = studentGrade.length > 0 ? studentGrade[0].grade : 'N/A';
-    return { ...student, grade };
-  });
-  return studentsWithGrades;
-}
+// /* Alternative solution: */
+// export default function updateStudentGradeByCity1(students, city, newGrades) {
+//   const studentsByCity = students.filter((student) => student.location === city);
+//   const studentsWithGrades = studentsByCity.map((student) => {
+//     const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
+//     const grade = studentGrade.length > 0 ? studentGrade[0].grade : 'N/A';
+//     return { ...student, grade };
+//   });
+//   return studentsWithGrades;
+// }
