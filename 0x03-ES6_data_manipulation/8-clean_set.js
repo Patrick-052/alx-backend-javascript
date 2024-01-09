051436a9cf1e763ef2212d9f79a4fa2620bcad3f
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-10 01:51:55 +0300

    Update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,25 @@
 /* Function to return a string of
 the set values separated by a hyphen (-) */
 
-export default function cleanSet(set, startString) {
-  const array = [];
-  set.forEach((item) => {
-    if (item.startsWith(startString)) {
-      if (startString === '') return '';
-      array.push(item.slice(startString.length));
-    }
+export default function cleanSet(_set, startString) {
+  if (typeof _set !== 'object') return '';
+  if (typeof startString !== 'string') return '';
+  if (startString.length === 0) return '';
+
+  const stringSet = [];
+  [..._set].forEach((x) => {
+    if (x && x.indexOf(startString) === 0) stringSet.push(x.replace(startString, ''));
   });
-  return array.join('-');
+  return stringSet.join('-');
 }
+
+// export default function cleanSet(set, startString) {
+//   const array = [];
+//   set.forEach((item) => {
+//     if (item.startsWith(startString)) {
+//       if (startString === '') return '';
+//       array.push(item.slice(startString.length));
+//     }
+//   });
+//   return array.join('-');
+// }
