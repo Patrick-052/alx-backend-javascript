872885578af86d99eb71ea1b188d0e8691bc5fba
Patrick-052 <muiruripatrick052@gmail.com>
2024-01-10 01:30:49 +0300

    Weak map

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,8 @@
+/* WeakMap Operations */
+
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+}
